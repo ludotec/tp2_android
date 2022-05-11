e595dc4fa19897963c9cb48614ba9402008c6a93
Claudio Fuentes <claudiofuentez6@gmail.com>
2022-05-10 23:19:27 -0300

    create land activity's

--- app/src/main/java/ste/com/tp_2/MainActivity.java
+++ app/src/main/java/ste/com/tp_2/MainActivity.java
@@ -3,12 +3,17 @@ package ste.com.tp_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnMainActivityAceptar , btnMainActivityCancelar;
+    private EditText edtMainActivityNombre;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_secondary);
     }
+
 }
